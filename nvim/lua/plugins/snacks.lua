569701ff7b24cf7a87389e97338ae7778ff91aac
Cristóbal Mardones Bucarey <cmardone@gmail.com>
2025-04-28 20:03:21 -0400

    Remove commented code and add buffers menu option

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -28,51 +28,17 @@ return {
       desc = 'Buffer delete other',
       mode = 'n',
     },
-    {
-      '<leader>bz',
-      function()
-        Snacks.zen()
-      end,
-      desc = 'Toggle Zen Mode',
-      mode = 'n',
-    },
   },
+  config = function()
+    local wk = require 'which-key'
+    wk.add({
+      { '<leader>b', group = 'buffers', desc = '+Buffers', mode = 'n' },
+    }, { prefix = '<leader>' })
+  end,
   opts = {
     dashboard = {
       preset = {
         pick = nil,
-        ---@type snacks.dashboard.Item[]
-        -- keys = {
-        --   { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
-        --   { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
-        --   {
-        --     icon = ' ',
-        --     key = 'g',
-        --     desc = 'Find Text',
-        --     action = ":lua Snacks.dashboard.pick('live_grep')",
-        --   },
-        --   {
-        --     icon = ' ',
-        --     key = 'r',
-        --     desc = 'Recent Files',
-        --     action = ":lua Snacks.dashboard.pick('oldfiles')",
-        --   },
-        --   {
-        --     icon = ' ',
-        --     key = 'c',
-        --     desc = 'Config',
-        --     action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-        --   },
-        --   { icon = ' ', key = 's', desc = 'Restore Session', section = 'session' },
-        --   {
-        --     icon = '󰒲 ',
-        --     key = 'l',
-        --     desc = 'Lazy',
-        --     action = ':Lazy',
-        --     enabled = package.loaded.lazy ~= nil,
-        --   },
-        --   { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
-        -- },
         header = [[
                                                                              
                ████ ██████           █████      ██                     
