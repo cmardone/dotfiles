c9321f9545c265f18f119c438b14460d4785c1ef
Cristóbal Mardones Bucarey <cmardone@gmail.com>
2025-04-29 22:26:48 -0400

    Add CodeCompanion and fix sorround config

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    'olimorris/codecompanion.nvim',
+    keys = {
+      { '<leader>cc', '<cmd>:CodeCompanionChat Toggle<CR>', 'Toggle Code Companion chat' },
+    },
+    opts = {
+      strategies = {
+        chat = {
+          adapter = 'gemini',
+        },
+        inline = {
+          adapter = 'gemini',
+          keymaps = {
+            accept_change = {
+              modes = { n = '<leader>a' },
+              description = 'Accept the suggested change',
+            },
+            reject_change = {
+              modes = { n = '<leader>r' },
+              description = 'Reject the suggested change',
+            },
+          },
+        },
+        cmd = {
+          adapter = 'gemini',
+        },
+      },
+    },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+  },
+}
